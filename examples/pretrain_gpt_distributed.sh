03946c851d7d35c99b3addbc06005a8684619acc
Varun Talwar <varun.talwar@sambanovasystems.com>
2021-08-12 09:51:32 +0000

    pretrain gpt distributed bash script updated

--- examples/pretrain_gpt_distributed.sh
+++ examples/pretrain_gpt_distributed.sh
@@ -10,29 +10,26 @@ NNODES=1
 NODE_RANK=0
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 
-DATA_PATH=<Specify path and file prefix>_text_document
-CHECKPOINT_PATH=<Specify path>
+DATA_PATH=/workspace/Megatron-LM/my-gpt2_text_document
 
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
 
 python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        pretrain_gpt.py \
-       --num-layers 24 \
-       --hidden-size 1024 \
-       --num-attention-heads 16 \
-       --micro-batch-size 8 \
-       --global-batch-size 64 \
+       --num-layers 48 \
+       --hidden-size 1600 \
+       --num-attention-heads 25 \
+       --micro-batch-size 4 \
+       --global-batch-size 512 \
        --seq-length 1024 \
        --max-position-embeddings 1024 \
-       --train-iters 500000 \
-       --lr-decay-iters 320000 \
-       --save $CHECKPOINT_PATH \
-       --load $CHECKPOINT_PATH \
+       --train-iters 1000 \
+       --lr-decay-iters 320 \
        --data-path $DATA_PATH \
-       --vocab-file gpt2-vocab.json \
-       --merge-file gpt2-merges.txt \
+       --vocab-file /workspace/Megatron-LM/gpt2-vocab.json \
+       --merge-file /workspace/Megatron-LM/gpt2-merges.txt \
        --data-impl mmap \
-       --split 949,50,1 \
+       --split 800,100,100 \
        --distributed-backend nccl \
        --lr 0.00015 \
        --lr-decay-style cosine \
@@ -41,8 +38,8 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        --clip-grad 1.0 \
        --lr-warmup-fraction .01 \
        --checkpoint-activations \
-       --log-interval 100 \
+       --log-interval 1 \
        --save-interval 10000 \
-       --eval-interval 1000 \
+       --eval-interval 10 \
        --eval-iters 10 \
        --fp16

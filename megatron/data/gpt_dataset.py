a35d9ea91290c3edd6a49ae8a6f755c7250d553a
Varun Talwar <varun.talwar@sambanovasystems.com>
2021-08-12 09:53:33 +0000

    small fix - indentation error

--- megatron/data/gpt_dataset.py
+++ megatron/data/gpt_dataset.py
@@ -240,7 +240,7 @@ def _build_index_mappings(name, data_prefix, documents, sizes,
                     'last epoch number of samples should be non-negative.'
                 num_samples_per_epoch = (tokens_per_epoch - 1) // seq_length
                 # assert last_epoch_num_samples < (num_samples_per_epoch + 1), \
-                    'last epoch number of samples exceeded max value.'
+                #    'last epoch number of samples exceeded max value.'
                 # If we have less than 80% of the samples for the last epoch,
                 # seperate out the epoch and treat it differently.
                 # Note: the 80% number is just based on common sense and can

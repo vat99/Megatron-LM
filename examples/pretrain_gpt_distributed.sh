ae83f56f0e73547ea74d80086884c9866cc9dfae
Varun Talwar <varun.talwar@sambanovasystems.com>
2021-08-17 05:21:02 +0000

    saving

--- examples/pretrain_gpt_distributed.sh
+++ examples/pretrain_gpt_distributed.sh
@@ -2,7 +2,7 @@
 
 # Runs the "345M" parameter model
 
-GPUS_PER_NODE=1
+GPUS_PER_NODE=8
 # Change for multinode config
 MASTER_ADDR=localhost
 MASTER_PORT=6000
@@ -19,8 +19,8 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        --num-layers 48 \
        --hidden-size 1600 \
        --num-attention-heads 25 \
-       --micro-batch-size 8 \
-       --global-batch-size 64 \
+       --micro-batch-size 16 \
+       --global-batch-size 512 \
        --seq-length 1024 \
        --max-position-embeddings 1024 \
        --train-iters 1000 \
